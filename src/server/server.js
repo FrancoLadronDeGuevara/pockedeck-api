a1acb012fb3dd4c59fd52dbc7f7c3e0c4863153d
Franco Guevara <francoguevara93@gmail.com>
2024-03-04 04:17:57 -0300

    Change url cors

--- src/server/server.js
+++ src/server/server.js
@@ -6,13 +6,21 @@ const { serverConfig } = require('../config/config');
 const port = serverConfig.port || 8000;
 const morgan = require('morgan');
 const cors = require('cors');
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 const errorHandlerMiddleware = require('../middlewares/errorHandler');
 
-app.use(cors({
+app.use(
+  cors({
     origin: 'https://pokedeck-game.vercel.app',
-    credentials: true
-  }));
+    credentials: true,
+  }),
+);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://pokedeck-game.vercel.app');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 //middlewares
 app.use(morgan('dev'));
@@ -32,10 +40,9 @@ app.use('/api/cards', cardsRoutes);
 app.use('/api/chests', chestsRoutes);
 app.use('/api/minigames', minigamesRoutes);
 
-
 //middleware error handler
 app.use(errorHandlerMiddleware);
 
 app.listen(port, () => {
-    console.log(`Estamos escuchando el puerto ${port}`);
+  console.log(`Estamos escuchando el puerto ${port}`);
 });
