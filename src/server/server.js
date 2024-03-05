cbfd3e41960922fdd2b7464252639c6eee53b6f0
Franco Guevara <francoguevara93@gmail.com>
2024-03-04 23:03:02 -0300

    Change url cors

--- src/server/server.js
+++ src/server/server.js
@@ -10,20 +10,19 @@ const cookieParser = require('cookie-parser');
 const errorHandlerMiddleware = require('../middlewares/errorHandler');
 
 const whitelist = ['http://localhost:5173', 'https://pokedeck-game.vercel.app', 'https://dashboard.uptimerobot.com/monitors/796504068'];
-const corsOptions = {
-  credentials: true,
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(console.error(`🟥 Not allowed by CORS -> ${origin}`));
-    }
-  },
-};
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Acceso CORS no permitido'));
+      }
+    },
+    credentials: true
+  }));
 
 //middlewares
 app.use(morgan('dev'));
-app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
